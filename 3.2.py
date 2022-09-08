2e5041e944c1716b887130fbcf30fcf889834917
TatsinaKV <ktvkaa3237@gmail.com>
2022-09-08 21:07:50 +0300

    add solution

---
+++ 3.2.py
@@ -0,0 +1,8 @@
+# Пользователь вводит 3 числа, найти среднее арифмитическое с
+# точность 3
+
+first_number = float(input('Введите первое число: '))
+second_number = float(input('Введите второе число: '))
+third_number = float(input('Введите третье число: '))
+arithmetic_mean = (first_number + second_number + third_number) / 3
+print(round(arithmetic_mean, 3))

85b53d59923ceef56db36e2b2880c23f8be5fb4f
TatsinaKV <ktvkaa3237@gmail.com>
2022-09-18 17:34:25 +0300

    add solution

--- 3.4.py
+++ 3.4.py
@@ -1,12 +1,22 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных
 # и сколько отрицательных
 
-first_number = input("Введите первое число: ")
-second_number = input('Введите второе число: ')
-third_number = input('Введите третье число: ')
-numbers = first_number + second_number + third_number
+first_number = -5
+second_number = -3
+third_number = 0
+numbers = str(first_number) + str(second_number) + str(third_number)
+print(numbers)
 negative_number = numbers.count('-')
 pozitive_number = 3 - negative_number
 print('отрицательных чисел: ', negative_number)
 print('положительных чисел: ', pozitive_number)
+
+a = [3, 0, -3, 0]
+poz = sum(i>0 for i in a)
+neg = sum(i<0 for i in a)
+print(poz)
+print(neg)
+
+
+
 
---
+++ 4.1.py
@@ -0,0 +1,4 @@
+# заполнить список степенями числа 2 (от 2^1 до 2^n)
+
+numbers = [2**i for i in range(1, int(input())+1)]
+print(numbers)

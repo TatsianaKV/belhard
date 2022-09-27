881b1cc805d2f1533dc27a20af1dd953ba1a0f6f
TatsinaKV <ktvkaa3237@gmail.com>
2022-09-27 13:50:45 +0300

    add solution

--- 6.5.py
+++ 6.5.py
@@ -1,10 +1,12 @@
 # Дан список чисел, необходимо его развернуть без использования метода revese и
 # функции reversed, а так же дополнительного списка и среза
+def numbers_list(numbers: list) -> list:
+    c = 0
+    for i in range(len(numbers)-1):
+        b = numbers.pop(len(numbers)-1)
+        numbers.insert(c, b)
+        c += 1
+    return numbers
 
-a = [1, 2, 3, 4, 5, 6]
-c = 0
-for i in range(len(a)-1):
-    b = a.pop(len(a)-1)
-    a.insert(c, b)
-    c += 1
-print(a)
+
+print(numbers_list([1, 3, 6, 4]))

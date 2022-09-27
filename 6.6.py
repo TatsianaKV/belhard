768cd9921f86b579893ef91ad0a823c26b448b79
TatsinaKV <ktvkaa3237@gmail.com>
2022-09-27 13:52:16 +0300

    add solution

---
+++ 6.6.py
@@ -0,0 +1,14 @@
+#Дан список рандомных чисел, необходимо отсортировать его в виде, сначала
+# четные, потом нечётные
+
+def sorting_even_odd_numbers(numbers: list) -> list:
+    even_numbers = []
+    odd_numbers = []
+    for i in numbers:
+        if i % 2:
+            odd_numbers.append(i)
+        else:
+            even_numbers.append(i)
+    return [even_numbers, odd_numbers]
+
+ print(sorting_even_odd_numbers([1, 3, 4, 2, 5, 6]))
